af7074f15fd6f819ff7b2bfddc286f4a9ea0d215
hadiaz <hadiaz@bancolombia.com.co>
2022-04-22 09:28:56 -0500

    Commit Delete method correccion urls

--- api/urls.py
+++ api/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('empresas/delete/<int:pk>', views.delete_empresas, name='home'),
     path('obligaciones/', views.view_obligaciones, name='home'),
     path('obligaciones/create/', views.create_view_obligaciones, name='home'),
-    path('obligaciones/delete/', views.delete_obligaciones, name='home'),
+    path('obligaciones/delete/<int:pk>', views.delete_obligaciones, name='home'),
     path('pagos/', views.create_view_pagos, name='home'),
     path('pagos/create/', views.create_view_pagos, name='home'),
-    path('pagos/delete/', views.delete_pagos, name='home'),
+    path('pagos/delete/<int:pk>', views.delete_pagos, name='home'),
 ]
 
